7488bce187b7348d047f52a29aed806ecce55c87
Lex Southin-Webb <lexwebb@gmail.com>
2023-04-08 11:47:00 +0100

    Add storybook

---
+++ components/Button.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Button from "./Button";
+
+// More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
+const meta: Meta<typeof Button> = {
+  title: "Components/Button",
+  component: Button,
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+  args: {
+    children: "Button",
+  },
+};
