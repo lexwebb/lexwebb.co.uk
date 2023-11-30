453ed6586d794a0468daa77dc0f6f6b541a9ae1a
Lex Southin-Webb <lexwebb@gmail.com>
2023-11-30 22:52:39 +0000

    Updated all deps

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["i.ytimg.com"],
+    remotePatterns: [
+      {
+        hostname: "i.ytimg.com",
+      },
+    ],
   },
 };
 
